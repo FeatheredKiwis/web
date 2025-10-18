6158961f95183863a9fd1f61d6558b5425696add
FeatheredKiwis <featheredkiwis@gmail.com>
2025-10-19 00:56:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ document.addEventListener("contextmenu", (event) => event.preventDefault());
 
 const games = {
   "game-card-pvb": { universeId: 8942147110 },
-  "game-card-sab": { universeId: 8841940437 },
+  "game-card-sab": { universeId: 8974465312 },
   "game-card-sac": { universeId: 8564865504 },
   "game-card-sap": { universeId: 8122648570 },
 };
@@ -274,5 +274,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   statsIntervalId = setInterval(fetchGameStats, 10000);
 });
+
 
 
